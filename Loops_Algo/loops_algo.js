f9a304130656f08fb6615536f3153cfeba746200
John Yarab <jyarab@gmail.com>
2021-05-17 15:11:32 -0400

    Add files via upload

---
+++ Loops_Algo/loops_algo.js
@@ -0,0 +1,12 @@
+for(var i = 1; i < 20; i++){
+    if(i % 2 != 0){
+        console.log(i)
+    }
+}
+
+var sum = 0;
+
+for(var x = 1; x <=5; x++){
+    sum += x;
+    console.log("Num: " + x,  "Sum: " + sum)
+    }
